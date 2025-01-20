9f8f707313c1b63d61d63108fe3fd7bcb83c2c26
Kalyani Sehgal <kalyani.sehgal.2910@outlook.com>
2025-01-20 20:03:48 +0530

    Challenge 2: Check for Palindrome String

---
+++ src/02 Identify a Palindrome/palindrome.py
@@ -0,0 +1,21 @@
+import re
+
+def is_palindrome(strtext):
+  #Check whether the input is string or not
+  if isinstance(strtext, str) == False:
+    return False
+  
+  #Check for the palindrome
+  cleanstring = re.sub(r'[^a-zA-Z0-9]', '', strtext)
+  revstring = cleanstring[::-1]
+
+  return True if cleanstring.lower() == revstring.lower() else False
+    
+
+print(is_palindrome('Go hang a salami, I’m a lasagna hog.'))
+print(is_palindrome('hello world'))
+print(is_palindrome(2112))
+print(is_palindrome(12.21))
+print(is_palindrome('A man, a plan, a canal: Panama!'))
+  
+  
